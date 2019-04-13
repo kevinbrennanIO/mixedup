4e110d11fa928c3b67cdc1a8ee524e67fcac9dd3
x16105834 <x16105834@student.ncirl.ie>
2019-04-13 15:22:41 +0100

    firebase login/logout/signup/statecheck

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,12 @@ var express = require("express");              // call the express module which 
 
 var app = express();                           // now we need to declare our app which is the invoked express application
 
-require('dotenv').config({ path: '/Users/kevinbrennan/Desktop/mixedup/vars.env' }); 
+require('dotenv').config(); 
 const path  = require('path');
 const VIEWS = path.join(__dirname, 'views');
 app.use(express.static("css"));
 app.use(express.static("images"));
+app.use(express.static("scripts"));
 app.set("view engine", "ejs");
 
 // direct application to the root page (index)
@@ -19,10 +20,14 @@ console.log("Site was acceesed");
 // direct application to the home page (home)
 app.get("/home", function(req, res) {
     res.render("home",{ROOT: "views"}); 
-    console.log("/home was acceesed");  
+    console.log("/home was accessed");  
 });
 
-
+// direct application to the signup page (signupForm)
+app.get("/signupForm", function(req, res) {
+    res.render("signupForm",{ROOT: "views"}); 
+    console.log("/signup form was acceesed");  
+});
 
 // this code provides the server port for our application to run on
 app.listen(process.env.PORT || 4000, process.env.IP || "0.0.0.0", function() {
