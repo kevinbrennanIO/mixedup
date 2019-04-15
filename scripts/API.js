270474b435fedd01f45ef4a677283b3feb24e7ae
Shane <shanemc138@hotmail.com>
2019-04-15 18:05:02 +0100

    APi
    Done

--- scripts/API.js
+++ scripts/API.js
@@ -1,16 +1,14 @@
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
-
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
+
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://www.thecocktaildb.com/api/json/v1/1/random.php', true);
+request.send();
 request.onload = function () {
 
   // Begin accessing JSON data here
@@ -21,9 +19,11 @@ request.onload = function () {
   if (request.status >= 200 && request.status < 400) {
 for (var i = 0; i < data.drinks.length; i++) {
     // key here is the index of line array
-       container.innerHTML += "Drink Name: "+ data.drinks[i]["strDrink"];
+    container.innerHTML += "Drink Name: "+ data.drinks[i]["strDrink"]+ "Drink Type : " + data.drinks[i]["strAlcoholic"]+ "Glass Type: " +data.drinks[i]["strGlass"]+ "Instructions: " +data.drinks[i]["strInstructions"]+ "Ingredients: " +data.drinks[i]["strIngredient1"]+ data.drinks[i]["strIngredient2"]+ data.drinks[i]["strIngredient3"]+ data.drinks[i]["strIngredient4"]+ data.drinks[i]["strIngredient5"]+ data.drinks[i]["strIngredient6"]+ "Measures: " +data.drinks[i]["strMeasure1"]+ data.drinks[i]["strMeasure2"]+ data.drinks[i]["strMeasure3"]+ data.drinks[i]["strMeasure4"]+ data.drinks[i]["strMeasure5"];
+    
     // each element of line array is an object
     // so we can iterate over its properties
+    // 
 }
   } else {
     const errorMessage = document.createElement('marquee');
@@ -32,4 +32,5 @@ for (var i = 0; i < data.drinks.length; i++) {
   }
 }
 
-request.send();
+
+
